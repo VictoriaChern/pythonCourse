8094352e30e3a8af8854cb4d489e1176538dea73
Viktoria.Cherniavskaya <victoria.kli1707@gmail.com>
2022-07-20 18:26:40 +0200

    add task4

---
+++ Lesson3/Task4.py
@@ -0,0 +1,23 @@
+import os
+
+# очищаем консоль
+os.system('clear')
+
+def ask_number():
+    number = input(f'Введите число: ')
+    if(not number.lstrip('+ -').isdigit()):
+        print('Ошибка: вы ввели не число! Повторите ввод')
+        return ask_number()
+    return int(number)
+
+def to_binary(number):
+    result = ''
+    while(number >= 2):
+        result += str(number%2)
+        number = int(number/2)
+    result += str(number)
+    return result[::-1]
+
+num = ask_number()
+print(f'{num} -> {to_binary(num)}')
+
