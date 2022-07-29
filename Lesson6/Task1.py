a18f5bf854e7871b619757b2759fd2bec37a5b02
Viktoria.Cherniavskaya <victoria.kli1707@gmail.com>
2022-07-29 20:28:29 +0200

    add Task1

---
+++ Lesson6/Task1.py
@@ -0,0 +1,19 @@
+import os
+
+# очищаем консоль
+os.system('clear')
+
+def ask_number():
+    number = input(f'Введите число: ')
+    if(not number.isdigit()):
+        print('Ошибка: вы ввели не число! Повторите ввод')
+        return ask_number()
+    return int(number)
+
+ls = ['aaa','aaa1', 'bbb2', 'ccc3', 'ddd4', 'eee5', '67']
+print(f'Список строк: {ls}')
+n = ask_number()
+fl = list(filter(lambda s: str(n) in s, ls))
+res = 'есть' if len(fl) != 0 else 'отсутствует'
+print(f'В заданном списке строк число {n} {res}')
+
